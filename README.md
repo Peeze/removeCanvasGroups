9da24d74bdf096a92152398723b85f44146d1e24
Peeze <pepe@artundimage.de>
2021-01-02 15:41:58 +0100

    Initial commit. Basic functionality.
    Add unlicense and README.
    
    Fix comment.
    
    Remove course list.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Remove Canvas Groups
+Firefox extension to remove group items from the sidebar in Canvas.
---
+++ removeCanvasGroups.js
@@ -0,0 +1,45 @@
+// Class names for elements to be modified
+LiClassName = "fOyUs_bGBk dxCCp_bGBk dxCCp_fLbg dxCCp_ycrn dxCCp_cfzP dxCCp_bCcs"; // List entries in group list
+AClassName = "fOyUs_bGBk fbyHH_bGBk fbyHH_bSMN"; // Links to group pages
+
+// List of group names to be removed
+const removeGroups = [];
+
+function observeNavTray() {
+    try {
+        // Select the node that will be observed for mutations
+        const NavTrayPortal = document.getElementById("nav-tray-portal");
+
+        // Options for the observer (which mutations to observe)
+        const config = { attributes: false, childList: true, subtree: true };
+
+        // Callback function to execute when mutations are observed
+        const callback = function(mutationsList, observer) {
+            // Get all group links
+            groupLinks = document.getElementsByClassName(AClassName);
+
+            // Give every entry a red border
+            for (groupLink of groupLinks) {
+                if (removeGroups.indexOf(groupLink.innerHTML) >= 0) {
+                    console.log("Remove " + groupLink.innerHTML);
+                    groupLink.parentNode.remove();
+                }
+                //groupLink.parentNode.remove();
+            }
+        }
+
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+
+        // Start observing the target node for configured mutations
+        observer.observe(NavTrayPortal, config);
+
+        // Later, you can stop observing
+        //observer.disconnect();
+    } catch(e) {
+        console.error(e.message);
+    }
+};
+
+var groupsButton = document.getElementById("global_nav_groups_link");
+groupsButton.onclick = observeNavTray;
